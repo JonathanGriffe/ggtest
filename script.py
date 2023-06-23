32b820bfed025877a0dfddbd1bf6f06b1be64495
Jonathan Griffe <jonathan.griffe@gitguardian.com>
2023-06-23 17:41:09 +0200

    trying akamai key

--- script.py
+++ script.py
@@ -1,2 +1,7 @@
-apikey = 'aio_oUQht70x5S3mcWwePPX7dJ9rYE7V'
-username = 'usernameKPKErRYU	 	 [[	,' 		"	  (	)'	["'	[''"(u'sNwF3cRoZFdQIg'
+client_token = 'client}E/ALur+VGds;!jMCmuT<iKad5g|6/?F>GKkC%hbgXCE&~;^L Yj
+{aT*wOI4`Z?w*brr                                                          *
+U<z>y6,+U{3akab-6MEp05hPpA6dj60y-vj90DjnS6yCBZHDg'
+secret_token = 'secretgKQ#X{7(RLW61o9/26Q4K1U6FRjatkeyRscpzjf5O-c4X5+jufHw10d6CSy-CSKu-qhn0='
+host = 'j/nAat1qgB7eG54zqKfkOTYHasg3.3Q9E8wNS4PD/iwe7.-w9Yyni7WlEwV0rztLcpvdZEeF68qw3VkrDyW9PZi1fZxOD.akamaiapis.net'
+Vuakab-JUXiovLO0eFlGbfO-8sipzOXIZJoXbNNg'hXTHLay07'$wdGy0zikE
+
